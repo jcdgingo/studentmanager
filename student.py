bb284ca5c6a2282e7956d382450ca7fbc0e3f5b1
Jess <34352838+jcdgingo@users.noreply.github.com>
2019-06-26 15:58:49 +0800

    Added files
    To be added with HTML file

---
+++ student.py
@@ -0,0 +1,27 @@
+students = []
+
+
+class Student:
+
+    school_name = "Springfield Elementary"
+
+    def __init__(self, name, last_name, student_id=000):
+        self.name = name
+        self.last_name = last_name
+        self.student_id = student_id
+        students.append(self)
+
+    def __str__(self):
+        return "Student " + self.name
+
+    def get_name_capitalize(self):
+        return self.name.capitalize()
+
+    def get_school_name(self):
+        return self.school_name
+
+
+mark = Student("Jessiedee Mark", "Gingo", 101187405)
+elma = Student("Elma", "Donaire", 96487)
+jeniza = Student("Jeniza", "Hortizano", 1023457823)
+print(students[0])
